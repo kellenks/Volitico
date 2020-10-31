1018f92f39080dc5b40f5e59b41ab4840ce60af4
Jesse Aviv <jesseaviv@gmail.com>
2020-10-31 14:32:59 -0400

    hi it's me

--- Extension/popup.js
+++ Extension/popup.js
@@ -5,6 +5,15 @@ document.addEventListener('DOMContentLoaded', function () {
     const div = document.createElement('div')
     div.textContent = `${url}: ${bg.notifi[url]}`
     document.body.appendChild(div)
+
+document.addEventListener('DOMContentLoaded', () => {
+   var y = document.getElementById("index_link");
+   y.addEventListener("click", openIndex);
+});
+
+function openIndex() {
+ chrome.tabs.create({active: true, url: "http://www.google.com"});
+}
   })
 
 }, false)
